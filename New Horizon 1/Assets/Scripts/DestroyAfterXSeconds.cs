da200dd6d70b4af53742458bdc05659671b78f97
Will Chaney <wills.apps@yahoo.com>
2018-03-04 12:20:09 -0700

    Particles now get destroyed

---
+++ New Horizon 1/Assets/Scripts/DestroyAfterXSeconds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this script to any Gameobject that will be destroyed X seconds after instantiations
+/// </summary>
+public class DestroyAfterXSeconds : MonoBehaviour
+{
+    public float TimeBeforeDestroying;
+    void Start()
+    {
+        Destroy(gameObject, TimeBeforeDestroying);
+    }
+}
