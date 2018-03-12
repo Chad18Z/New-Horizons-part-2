b63ec043b2670373f860baac24f0aae42ce6cddf
Dylan Updegrave <dylanupde@gmail.com>
2018-03-12 01:02:32 -0600

    Added more enemy animations, added new vein walls

--- New Horizon 1/Assets/Scripts/Enemy.cs
+++ New Horizon 1/Assets/Scripts/Enemy.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Enemy : Cell
 {
+    [SerializeField] RuntimeAnimatorController enemyAnimator1;
+    [SerializeField] RuntimeAnimatorController enemyAnimator2;
+    [SerializeField] RuntimeAnimatorController enemyAnimator3;
+
     // Use this for initialization
     protected override void Start()
     {
+        int randomNum = Random.Range(1, 4);
+        switch (randomNum)
+        {
+            case 1:
+                GetComponent<Animator>().runtimeAnimatorController = enemyAnimator1;
+                break;
+            case 2:
+                GetComponent<Animator>().runtimeAnimatorController = enemyAnimator2;
+                break;
+            case 3:
+                GetComponent<Animator>().runtimeAnimatorController = enemyAnimator3;
+                break;
+            default:
+                break;
+        }
+
         base.Start();
     }
 
