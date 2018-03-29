fdfc8203e4213420afa9c176e5a0ffd70d119663
Chad18Z <credeurchad@gmail.com>
2018-03-28 20:01:59 -0600

    Dummy enemy UI added to scene. This is only for testing!

--- New Horizon 1/Assets/Scripts/Player.cs
+++ New Horizon 1/Assets/Scripts/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject missile;
     [SerializeField] float fireCooldownTime = .5f;
     [SerializeField] float totalAssaultTime = 4f;
-    [Range(0f, 1f)] [SerializeField] float maxInflation;
+    [Range(0f, 2f)] [SerializeField] float maxInflation;
 
     float cytoSpeed = 35f; // speed at which cytoburst travels
 
@@ -36,6 +36,8 @@ public class Player : MonoBehaviour
     bool gotSpeed = false;
     bool gotCyto = false;
     bool gotBurst = false;
+
+    float amountToInflate;
 
     //Timer components 
     Timer powerupTimer;
@@ -156,32 +158,10 @@ public class Player : MonoBehaviour
         }
     }
 
-
     // Update is called once per frame
     void Update()
     {
-        //float amountToInflate;
-
-        //// When the player holds down the mouse...
-        //if (Input.GetMouseButtonDown(0) && !stuckToEnemy)
-        //{
-        //    // ...record the time
-        //    chargeStartTime = Time.time;
-        //}
-
-        //// While the mouse is being held down...
-        //if (Input.GetMouseButton(1))
-        //{
-        //    // Set the charge amount to be how long in seconds the mouse was held down
-        //    chargeTimeCurrent = Time.time - chargeStartTime;
-        //    chargeTimeCurrent = Mathf.Clamp(chargeTimeCurrent, 0f, maxChargeTime);
-
-        //    //// Calculate how much to inflate based off the max amount allowed and the current charge time
-        //    //amountToInflate = 1 + (chargeTimeCurrent / maxChargeTime) * maxInflation;
-
-        //    //// Inflate that much
-        //    //transform.localScale = new Vector3(normalScale.x * amountToInflate, normalScale.y * amountToInflate, normalScale.z);
-        //}
+       if (Input.GetMouseButtonDown(0)) { Fire(); }
 
         //// When the player releases the mouse button...
         if (Input.GetMouseButton(1))
@@ -194,79 +174,8 @@ public class Player : MonoBehaviour
 
             // Shoot the player in that direction, with the magnitude of the thrust multiplier times charge amount
             rb2d.AddForce(directionToGo.normalized * thrustMultiplier * Time.deltaTime, ForceMode2D.Impulse);
-
-            transform.localScale = normalScale;
         }
-
-        // When player fires 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 cytoFireDirection = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)cytoMountPoint.transform.position).normalized;
-
-            GameObject cyto = Instantiate(cytoBlobPrefab, cytoMountPoint.transform.position, Quaternion.identity);
-
-            Rigidbody2D cytoRb = cyto.GetComponent<Rigidbody2D>();
-
-            cytoRb.AddForce(cytoFireDirection * cytoSpeed, ForceMode2D.Impulse);
-
-        }
-
-
-
-        //if (stuckToEnemy)
-        //{
-        //    if (timeAssaultStarted == 0f)
-        //    {
-        //        timeAssaultStarted = Time.time;
-        //        InvokeRepeating("FireMissile", .5f, fireCooldownTime);
-        //    }
-        //    if (Time.time - timeAssaultStarted > totalAssaultTime)
-        //    {
-        //        stuckToEnemy = false;
-        //        timeAssaultStarted = 0f;
-        //        CancelInvoke();
-        //        rb2d.AddForce(guyImStuckToPositionDifference.normalized * 10, ForceMode2D.Impulse);
-        //    }
-        //}
     }
-
-    //void LateUpdate()
-    //{
-    //    if (guyImStuckTo && stuckToEnemy)
-    //    {
-    //        transform.position = guyImStuckTo.transform.position + guyImStuckToPositionDifference;
-    //    }
-    //}
-
-    //void OnCollisionStay2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject == guyImStuckTo)
-    //    {
-    //        stuckToEnemy = false;
-    //    }
-    //}
-
-    //void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Enemy")
-    //    {
-    //        guyImStuckTo = collision.gameObject;
-    //        guyImStuckToPositionDifference = transform.position - guyImStuckTo.gameObject.transform.position;
-    //        stuckToEnemy = true;
-    //    }
-    //}
-
-    //void FireMissile()
-    //{
-    //    Vector3 positionDifference = guyImStuckTo.transform.position - transform.position;
-    //    positionDifference.Normalize();
-
-    //    float rot_z = Mathf.Atan2(positionDifference.y, positionDifference.x) * Mathf.Rad2Deg;
-    //    Quaternion missileRotation = Quaternion.Euler(0f, 0f, rot_z);
-
-    //    GameObject createdMissile = Instantiate(missile, transform.position, missileRotation);
-    //    createdMissile.GetComponent<SeekerMissile>().target = guyImStuckTo;
-    //}
 
 	// called if mouse is over any child object inheriting cell
 	void OnMouseOver() {
@@ -279,4 +188,14 @@ public class Player : MonoBehaviour
 		this.playerInfo.SetMouseOver (false);
 	}
 
+    /// <summary>
+    /// This method fires a cytoblob
+    /// </summary>
+    void Fire()
+    {
+        Vector2 cytoFireDirection = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)cytoMountPoint.transform.position).normalized;
+        GameObject cyto = Instantiate(cytoBlobPrefab, cytoMountPoint.transform.position, Quaternion.identity);
+        Rigidbody2D cytoRb = cyto.GetComponent<Rigidbody2D>();
+        cytoRb.AddForce(cytoFireDirection * cytoSpeed, ForceMode2D.Impulse);
+    }
 }
